d608296cef63b7e199755e27e7990c91b2cb4cc1
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-10 02:40:55 +0100

    task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,12 +11,10 @@ def do_pack():
     archive_path = "versions/{}".format(archive_name)
     local("mkdir -p versions > /dev/null 2>&1")
     print("Packing web_static to {}".format(archive_path))
-    result = local("tar -czvf {} web_static \
-    > /dev/null 2>&1".format(archive_path))
+    result = local("tar -czvf {} web_static > /dev/null 2>&1".format(archive_path))
     if result.succeeded:
-        size = local("stat -c %s {} \
-        > /dev/null 2>&1".format(archive_path), capture=True)
-        print("web_static packed: {} -> {}Bytes".format(archive_path, size))
+        size = local("du -b {} | awk '{{print $1}}'".format(archive_path), capture=True)
+        print("web_static packed: {} -> {}Bytes".format(archive_path, size.strip()))
         return archive_path
     else:
         return None
