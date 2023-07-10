2e274c272dd64d1b3b67b9edbd086222ad9a0313
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-09 18:30:06 -0700

    task_1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,6 +10,7 @@ def do_pack():
     archive_name = "web_static_" + timestamp + ".tgz"
     archive_path = "versions/{}".format(archive_name)
     local("mkdir -p versions")
+    print("Packing web_static to {}".format(archive_path))
     result = local("tar -cvzf {} web_static".format(archive_path))
     if result.succeeded:
         size = local("stat -c %s {}".format(archive_path), capture=True)
