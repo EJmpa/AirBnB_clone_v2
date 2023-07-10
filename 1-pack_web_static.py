3e1357ebecbe01c1e0288f139c0828a3db42867c
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-09 18:08:05 -0700

    task_1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 from fabric.api import local
+from time import strftime
 from datetime import datetime
 
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+    timestamp = strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_" + timestamp + ".tgz"
     local("mkdir -p versions")
     result = local("tar -czvf versions/{} web_static".format(archive_name))
