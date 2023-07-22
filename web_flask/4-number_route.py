428709249935e000cd700b7b0985ab02c717a077
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-22 03:36:14 -0700

    task_4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,7 +27,8 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_with_text(text):
     """
-    This function defines the route '/c/<text>' and displays "C " followed by the value of the text variable
+    This function defines the route '/c/<text>' and displays "C "
+    followed by the value of the text variable
     (replace underscore _ symbols with a space).
     """
     return "C {}".format(text.replace("_", " "))
@@ -37,10 +38,22 @@ def c_with_text(text):
 @app.route('/python/<text>', strict_slashes=False)
 def python_with_text(text="is cool"):
     """
-    This function defines the route '/python/<text>' and displays "Python " followed by the value of the text variable
-    (replace underscore _ symbols with a space). If no text is provided, it defaults to "is cool".
+    This function defines the route '/python/<text>' and displays "Python "
+    followed by the value of the text variable
+    (replace underscore _ symbols with a space).
+    If no text is provided, it defaults to "is cool".
     """
     return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    This function defines the route '/number/<n>' and displays "<n>
+    is a number" only if n is an integer.
+    """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
